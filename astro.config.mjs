f5f43cc0b0a2b4ff0c70e6293097b23e85a36954
gretzky <sfed617@gmail.com>
2024-08-27 12:25:53 -0400

    add dotenvx post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://spencer.town",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [sitemap(), react(), robotsTxt(), mdx()],
   markdown: {
     shikiConfig: {
       theme: "ayu-dark",
